40bb17e66cea7dc452fbd7776f92f2c5c350c5dc
Jabir Ahmed Khan <97745600+Jab754@users.noreply.github.com>
2023-04-12 19:33:07 +0530

    Prototype

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-@login_required(login_url='login')
+#@login_required(login_url='login')
 def index(request):
     if request.method == 'POST':
         # Retrieve uploaded file from POST request
